455f9143d6d8abe71c81932cbd74c9454ca4d7f1
mango_gills <64761137+mango-gills@users.noreply.github.com>
2023-04-21 16:43:06 +0800

    test tailwind cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    <!-- @vite('resources/css/app.css') -->
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-gray-900 text-white">
-    <div class="container mx-auto px-4 font-nunito">
+<body class="text-white bg-gray-900">
+    <div class="container px-4 mx-auto font-nunito">
         @yield('content')
     </div>
 </body>
