1db81b82e186d7409fb2439362739178e50e9697
mango_gills <64761137+mango-gills@users.noreply.github.com>
2023-04-21 16:47:44 +0800

    test font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- @vite('resources/css/app.css') -->
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900;1000&display=swap");
+
+        body {
+            font-family: "Nunito", sans-serif;
+        }
+    </style>
 </head>
 
 <body class="text-white bg-gray-900">
