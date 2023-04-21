051c68101602d83f8d51033bd8c6c8e6ec3b5286
mango_gills <64761137+mango-gills@users.noreply.github.com>
2023-04-21 17:05:16 +0800

    remove tailwind cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- @vite('resources/css/app.css') -->
-    <script src="https://cdn.tailwindcss.com"></script>
-
-    <style>
-        @import url("https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900;1000&display=swap");
-
-        body {
-            font-family: "Nunito", sans-serif;
-        }
-    </style>
+    @vite('resources/css/app.css')
 </head>
 
 <body class="text-white bg-gray-900">
